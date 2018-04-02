36300a6b095a83cb97f6665382c871afaf4b9ee6
benmatheson <mathesonben@gmail.com>
2018-04-01 20:52:25 -0800

    initial commit

---
+++ js/main.js
@@ -0,0 +1,521 @@
+//images
+
+//https://www.google.com/imgres?imgurl=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2F1%2F1d%2FWhole_Foods_Markham_Canada.jpg&imgrefurl=https%3A%2F%2Fcommons.wikimedia.org%2Fwiki%2FFile%3AWhole_Foods_Markham_Canada.jpg&docid=Ot5ZiHOXmfaINM&tbnid=fiWdyiffo05mfM%3A&vet=10ahUKEwi6-83N7ZfaAhVN3WMKHbpNBwIQMwg8KAAwAA..i&w=3261&h=1708&bih=575&biw=1161&q=whole%20foods&ved=0ahUKEwi6-83N7ZfaAhVN3WMKHbpNBwIQMwg8KAAwAA&iact=mrc&uact=8
+//https://www.amazon.jobs/en/teams/amazon-robotics
+
+//https://www.amazon.com/Amazon-Prime-Air/b?ie=UTF8&node=8037720011
+// https://www.amazon.com/Amazon-Fire-Phone-32GB-Unlocked/dp/B00OC0USA6
+// https://www.flickr.com/photos/atomictaco/5095355117
+// https://commons.wikimedia.org/wiki/File:Amazon_Prime_logo.png
+// https://www.google.com/imgres?imgurl=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fthumb%2F1%2F1d%2FAmazonWebservices_Logo.svg%2F2000px-AmazonWebservices_Logo.svg.png&imgrefurl=https%3A%2F%2Fcommons.wikimedia.org%2Fwiki%2FFile%3AAmazonWebservices_Logo.svg&docid=g_a8Ru6h5Mk02M&tbnid=5mX3YxMl-tr5-M%3A&vet=10ahUKEwib6Njl2ZDaAhWJLmMKHeORCo4QMwhTKAAwAA..i&w=2000&h=752&bih=575&biw=1161&q=amazon%20aws&ved=0ahUKEwib6Njl2ZDaAhWJLmMKHeORCo4QMwhTKAAwAA&iact=mrc&uact=8
+//https://commons.wikimedia.org/wiki/File:Amazon_Espa%C3%B1a_por_dentro_(18).jpg
+
+// https://commons.wikimedia.org/wiki/Amazon_Kindle#/media/File:Kindle_2_-_Front.jpg
+//https://upload.wikimedia.org/wikipedia/commons/thumb/5/5c/Amazon_Echo.jpg/267px-Amazon_Echo.jpg
+
+//actual vis to make:
+
+//circle timelines for : platform, aws, book, kindle, fresh, 
+//like this http://insightextractor.com/2012/08/07/a-nice-data-visualization-on-users-twitter-text-data/
+
+//bar code type thing maybe of sentiment or joy?
+//https://www.google.com/search?rlz=1C1GGRV_enUS749US749&biw=1164&bih=835&tbm=isch&sa=1&ei=V0u0WvO5DNHKjwOl2pqIDA&q=text+visualizatoin&oq=text+visualizatoin&gs_l=psy-ab.3..0l3j0i30k1j0i8i30k1l4j0i24k1l2.43273.46198.0.46356.20.12.1.0.0.0.354.1180.0j4j1j1.6.0....0...1c.1.64.psy-ab..13.7.1182...0i67k1.0.vQo0Q-SzUis#imgrc=SPnnlaBLYvNs1M:
+	
+
+//td-if per year
+
+//customer/shareholder ratio
+//grocery thing
+
+
+
+
+
+// *add teh product hover thing to the list of VIS
+// *add screenshots to list vis
+// *add heavily annotated market map graph to list vis
+// *add polarity test? https://www.rdocumentation.org/packages/qdap/versions/2.2.9/topics/polarity
+
+
+const descriptions = {
+
+"groc": "same convenient home delivery on a great selection of prepared foods and specialty items. We’ll continue our methodical approach – measuring and refining Amazon Fresh – with the goal of bringing this incredible service to more cities over time.", 
+"kindle": "2007: If our tools make information snacking easier, we’ll shift more toward information snacking and away from long-form reading. Kindle is purpose-built for long-form reading. We hope Kindle and its successors may gradually and incrementally move us over years into a world with longer spans of attention, providing a counterbalance to the recent proliferation of info-snacking tools.",
+"phone": "also phone",
+"aws": "2010: ..advanced machine learning techniques provide more accurate classification and can self-heal to adapt to changing conditions. For example, our search engine employs data mining and machine learning algorithms that run in the background to build topic models, and we apply information extraction algorithms to identify attributes and extract entities from unstructured descriptions, allowing customers to narrow their searches and quickly find the desired product.",
+"dot": "Amazon's pushin",
+"wf": "We will have to wait until the 2017 letter comes out this spring to hear about the $13 billion dollar purchase of Whole Foods.",
+"prime": "2014:Ten years ago, we launched Amazon Prime, originally designed as an all-you-can-eat free and fast shipping program. We were told repeatedly that it was a risky move, and in some ways it was. In its first year, we gave up many millions of dollars in shipping revenue, and there was no simple math to show that it would be worth it. Our decision to go ahead was built on the positive results we’d seen earlier when we introduced Free Super Saver Shipping, and an intuition that customers would quickly grasp that they were being offered the best deal in the history of shopping. In addition, analysis told us that, if we achieved scale, we would be able to significantly lower the cost of fast shipping."
+
+}
+
+
+
+console.log(descriptions.groc)
+
+
+function app (product) {
+
+var desc = document.createElement('div')
+
+console.log(descriptions.product);
+desc.innerHTML = JSON.stringify(descriptions[product]);
+
+var currentDiv = document.getElementById("parent"); 
+
+ 
+document.body.insertBefore(desc, currentDiv); 
+desc.classList.add("abs");
+
+
+}
+
+
+function rem () {
+
+document.querySelector(".abs").remove();
+
+}
+
+
+function dim (e) {
+
+console.log("JESUS")
+
+// console.log(event.target);
+
+const current = event.target;
+const parent = document.getElementById('parent');
+const sibs = parent.children;
+// console.log(sibs[2].classList);
+
+// sibs[2].classList.add("darken");
+
+Array.from(sibs).forEach((e)=>
+e.classList.add("darken"));
+current.classList.remove("darken");
+const currData = current.getAttribute('data1');
+console.log(currData);
+app(currData);
+
+
+
+
+
+
+
+}
+
+function bright (e) {
+
+const current = event.target;
+const parent = document.getElementById('parent');
+const sibs = parent.children;
+// console.log(sibs[2].classList);
+
+Array.from(sibs).forEach((e)=>
+e.classList.remove("darken"));
+
+rem();
+
+}
+
+
+
+
+
+
+const margin = {top: 20, right: 20, bottom: 30, left: 50};
+
+ const stockWidth = 700 - margin.left - margin.right;
+ const stockHeight = 650 - margin.top - margin.bottom;
+
+
+const stockSvg = d3.select('#stock')
+	.append('svg')
+	.attr('width', stockWidth)
+	.attr('height', stockHeight);
+
+const stockSvgG = stockSvg.append("g");
+
+
+// const svgG = svg.append("g/");
+
+
+var parseTime = d3.timeParse("%Y-%m-%d");
+
+
+d3.csv("data/az_week.csv", function(d){
+	d.Date = parseTime(d.Date);
+	d.Close = +d.Close;
+	// console.log(d);
+	return d;
+
+}, function(error, data) {
+  if (error) throw error;
+
+const stockYEx = d3.extent(data, d=>d.Close);
+const stockXEx = d3.extent(data, d=>d.Date);
+
+
+
+
+const xStock = d3.scaleTime().domain(stockXEx).range([0, 600]);
+const yStock = d3.scaleLinear().domain(stockYEx).range([600, 0]);
+
+
+
+
+const stockLine = d3.line()
+					.x(d=>xStock(d.Date))
+					.y(d=> yStock(d.Close));
+
+	const stockLineDef = stockSvgG.append("path")
+      .datum(data)
+      .attr("stroke", "gray")
+      .attr("fill", "none")
+      // .attr("stroke-linejoin", "round")
+      // .attr("stroke-linecap", "round")
+      .attr("stroke-width", 2)
+      .attr("d", stockLine);
+
+
+
+	var stockLength = stockLineDef.node().getTotalLength();
+
+
+      stockLineDef.attr('stroke-dasharray', `${stockLength} ${stockLength}`)
+      .attr('stroke-dashoffset', stockLength)
+			.transition()
+  			.ease(d3.easeLinear)
+				.delay(10)
+				.duration(9000)
+                 .style("opacity", 1)
+                .attr('stroke-dashoffset', 0)
+
+
+stockSvgG.append('text')
+	.text("Amazon.com Stock Price")
+	.attr("x", xStock(parseTime("1999-05-10")))
+	.attr("y",yStock(300))
+		.attr("class", "center")
+		.attr("opacity", 0)
+		.transition()
+		.duration(500)
+		.delay(100)
+		.attr("opacity", 1)
+
+
+stockSvgG.append('text')
+	.text("1997")
+	.attr("x", xStock(parseTime("1997-09-30")))
+	.attr("y",yStock(35))
+	.attr("class", "anno")
+		.attr("opacity", 0)
+		.transition()
+		.delay(300)
+				.duration(500)
+
+		.attr("opacity", 1)
+
+
+stockSvgG.append('text')
+	.text("2008")
+	.attr("x", xStock(parseTime("2008-09-30")))
+	.attr("y",yStock(100))
+		.attr("class", "anno")
+		.attr("opacity", 0)
+		.transition()
+		.delay(2000)
+				.duration(500)
+
+		.attr("opacity", 1)
+
+
+
+
+	stockSvgG.append('text')
+	.text("2013")
+	.attr("x", xStock(parseTime("2013-01-30")))
+	.attr("y",yStock(500))
+		.attr("class", "anno")
+				.attr("opacity", 0)
+		.transition()
+				.duration(500)
+
+		.delay(5000)
+		.attr("opacity", 1)
+
+
+
+	stockSvgG.append('text')
+	.text("2017")
+	.attr("x", xStock(parseTime("2017-06-30")))
+	.attr("y",yStock(1000))
+		.attr("class", "anno")
+				.attr("opacity", 0)
+		.transition()
+		.duration(500)
+		.delay(7000)
+		.attr("opacity", 1)
+
+
+
+	stockSvgG.append('text')
+	.text("2018")
+	.attr("x", xStock(parseTime("2016-05-10")))
+	.attr("y",yStock(1500))
+		.attr("class", "anno")
+				.attr("opacity", 0)
+		.transition()
+		.duration(500)
+		.delay(8000)
+		.attr("opacity", 1)
+
+
+
+
+})
+
+
+
+
+
+
+
+
+
+d3.csv("data/productTerms.csv", function(data1){
+
+
+const prime = data1.filter(d=>d.word =="prime");
+const aws = data1.filter(d=>d.word =="aws");
+const book = data1.filter(d=>d.word =="book" || d.word=="books");
+const fresh = data1.filter(d=>d.word =="fresh");
+const kindle = data1.filter(d=>d.word =="kindle");
+const data = data1.filter(d=>d.word =="data");
+const relentless = data1.filter(d=>d.word =="relentless");
+const phone = data1.filter(d=>d.word =="phone");
+
+console.log(book);
+
+
+const x1 = d3.scaleLinear().domain([1997,2016]).range([20,620]);
+
+
+function term(el, data, color) {
+
+const svg = d3.select(el).append("svg")
+	.attr("width", 700)
+	.attr("height", 100)
+
+const svgG = svg.append("g");
+
+svgG.append('line')
+	.attr("x1", x1(1997))
+	.attr("x2", x1(2016))
+	.attr("y1", 50)
+	.attr("y2", 50)
+	.attr("stroke", 'lightgray')
+	.attr("stroke-width", .3);
+
+svgG.selectAll('circle')
+	.data(data)
+	.enter()
+	.append('circle')
+	.attr('cx', d=>x1(d.year))
+	.attr("cy", 50)
+	.attr('r', 2)
+	.attr("fill", color)
+	.attr("opacity", .5)
+	.transition()
+	.delay((d,i)=> i*100)
+	.attr('r', d=>Math.sqrt(d.n)*8)
+
+svgG.append("text")
+	.text("1997")
+	.attr('x', d=>x1(1997))
+	.attr("y", 80)
+	.style("font-size", 10)
+
+
+
+
+
+svgG.append("text")
+	.text("2017")
+	.attr('x', d=>x1(2017))
+	.attr("y", 80)
+		.style("font-size", 10)
+
+
+
+svgG.append("text")
+	.text("2005")
+	.attr('x', d=>x1(2005))
+	.attr("y", 80)
+		.style("font-size", 10)
+
+
+
+
+svgG.append("text")
+	.text("2010")
+	.attr('x', d=>x1(2010))
+	.attr("y", 80)
+		.style("font-size", 10)
+
+
+
+
+	}
+
+term("#term1", prime, "#472836")
+term("#term2", book, "#3D394D")
+term("#term4", kindle, "#244B58")
+term("#term3", fresh, "#195A50")
+term("#term5", relentless, '#3A653D')
+term("#term6", phone, "#666A2B")
+term("#term7", data, "#956831")
+term("#term8", aws, "darkorange")
+
+
+
+
+
+});
+
+const x2 = d3.scaleLinear().domain([1997,2016]).range([20,240]);
+
+const y2 = d3.scaleLinear().domain([-6, 7]).range([0,200]);
+
+
+
+d3.csv("data/letSent.csv", function(data){
+
+
+
+
+
+const y1997= data.filter(d=>d.year==1997)
+const y1998= data.filter(d=>d.year==1998)
+const y1999= data.filter(d=>d.year==1999)
+const y2000= data.filter(d=>d.year==2000)
+const y2001= data.filter(d=>d.year==2001)
+const y2002= data.filter(d=>d.year==2002)
+const y2003= data.filter(d=>d.year==2003)
+const y2004= data.filter(d=>d.year==2004)
+const y2005= data.filter(d=>d.year==2005)
+const y2006= data.filter(d=>d.year==2006)
+const y2007= data.filter(d=>d.year==2007)
+const y2008= data.filter(d=>d.year==2008)
+const y2009= data.filter(d=>d.year==2009)
+const y2010= data.filter(d=>d.year==2010)
+const y2011= data.filter(d=>d.year==2011)
+const y2012= data.filter(d=>d.year==2012)
+const y2013= data.filter(d=>d.year==2013)
+const y2014= data.filter(d=>d.year==2014)
+const y2015= data.filter(d=>d.year==2015)
+const y2016= data.filter(d=>d.year==2016)
+const y2017= data.filter(d=>d.year==2017)
+
+
+
+
+
+function sent(el, data, color, yr) {
+
+const svg = d3.select(el).append("svg")
+	.attr("width", 200)
+	.attr("height", 300)
+
+const svgG = svg.append("g");
+
+
+svgG.append('line')
+	.attr("x1", 0)
+	.attr("x2", 200)
+	.attr("y1", 200-y2(0))
+	.attr("y2", 200-y2(0))
+	.attr("stroke", 'lightgray')
+	.attr("stroke-width", .3);
+
+
+
+
+svgG.selectAll('rect')
+	.data(data)
+	.enter()
+	.append('rect')
+	.attr('x', (d,i)=>i*4)
+	.attr("y", d=>200-y2(0))
+		.attr("width",3)
+	.attr("fill", d=> d.sentiment>0 ? "#2C3B4C" : "#793E3C")
+	.attr("opacity", .5)
+	.transition()
+	.attr("transform", "translate(" + 20 + "," + 0 + ")")
+
+	.delay((d,i)=> i*50)
+	.attr("y", d=> 200-y2(Math.max(0, d.sentiment)))
+	.attr("height", d=> Math.abs((y2(d.sentiment)-y2(0)))) //LENGHT is  ABS value of 
+
+
+svgG.append('text')
+	.text(yr)
+	.attr("x", 10)
+	.attr("y", 20)
+	.attr("class", "anno")
+
+svgG.append('g')
+    // .attr("transform", "translate(" + -10 + "," + 0 + ")")
+	.append('text')
+	.text(0)
+	.attr("x", 0)
+	.attr("y", 200-y2(0))
+	.attr("class", "annoSent")
+
+
+svgG.append('text')
+	.text(3)
+	.attr("x", 0)
+	.attr("y", 200-y2(3))
+	.attr("class", "annoSent")
+
+
+
+}
+
+
+sent("#sentEl1", y1997, 'red', 1997)
+sent("#sentEl2", y1998, 'blue', 1998)
+sent("#sentEl3", y1999, 'blue', 1999)
+sent("#sentEl4", y2000, 'blue', 2000)
+sent("#sentEl5", y2001, 'blue',2001)
+sent("#sentEl6", y2002, 'blue', 2002)
+sent("#sentEl7", y2003, 'blue', 2003)
+sent("#sentEl8", y2004, 'blue', 2004)
+sent("#sentEl9", y2005, 'blue', 2005)
+sent("#sentEl10", y2006, 'blue', 2006)
+sent("#sentEl11", y2007, 'blue', 2007)
+sent("#sentEl12", y2008, 'blue', 2008)
+sent("#sentEl13", y2009, 'blue', 2009)
+sent("#sentEl13", y2010, 'blue', 2010)
+sent("#sentEl14", y2011, 'blue', 2011)
+sent("#sentEl15", y2012, 'blue', 2012)
+sent("#sentEl16", y2013, 'blue', 2013)
+sent("#sentEl17", y2014, 'blue', 2014)
+sent("#sentEl18", y2015, 'blue', 2015)
+sent("#sentEl19", y2016, 'blue',2016)
+
+
+
+
+
+
+
+
+
+})
+
+
+
